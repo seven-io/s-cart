ea6cc6ac72b2041f82ea29df36944fb38d21b61c
André Matthies <matthiez@gmail.com>
2022-10-13 15:22:50 +0200

    add german language

---
+++ Lang/de/lang.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'admin' => [
+        'title' => 'seven',
+    ],
+    'api_key' => 'API-Schlüssel',
+    'menu' => [
+        'heading' => 'seven',
+        'bulk_sms' => 'Massen-SMS',
+    ],
+    'filters' => [
+        'legend' => 'Filter',
+        'status' => [
+            'label' => 'Nur aktive Kundschaft',
+        ]
+    ],
+    'sms' => [
+        'bulk' => [
+            'heading' => 'Massen-SMS versenden',
+            'intro' => 'Diese Aktion versendet SMS an die gesamte Kundschaft gleichzeitig.',
+        ],
+        'debug' => [
+            'label' => 'Debug',
+        ],
+        'flash' => [
+            'label' => 'Flash',
+        ],
+        'from' => [
+            'label' => 'Absenderkennung',
+            'placeholder' => 'S-Cart'
+        ],
+        'foreign_id' => [
+            'label' => 'Foreign ID',
+            'placeholder' => 'Meine foreign ID'
+        ],
+        'label' => [
+            'label' => 'Label',
+            'placeholder' => 'Mein Label'
+        ],
+        'performance_tracking' => [
+            'label' => 'Performance Tracking',
+        ],
+        'text' => [
+            'label' => 'Nachrichteninhalt',
+            'placeholder' => 'Liebe Kundschaft. Als Dank erhalten Sie mit dem Code MINUS10 10% Rabatt auf Ihre nächste Bestellung. Danke für Ihr Vertrauen!'
+        ],
+    ],
+    'submit' => 'Absenden',
+    'title' => 'seven',
+];
